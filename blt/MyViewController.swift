2ae357ea3e40c5e43ce23f4b03709a1c39a93329
creator0221 <evan_success@qq.com>
2016-09-21 02:56:36 +0800

    09/21/16 02:56 成功使用 StoryBoard 创建 tableView 和Push 到下一个页面

---
+++ blt/MyViewController.swift
@@ -0,0 +1,32 @@
+import Foundation
+import UIKit
+
+class MyViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableV: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        
+//    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        myCell.textLabel?.text = "123"
+        
+        return myCell
+    }
+}
---
+++ blt/TwoViewController.swift
@@ -0,0 +1,18 @@
+import Foundation
+import UIKit
+
+class TwoViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+}
