6d6d30cb4d578f3c220a66e84cf28bc8732a4e79
creator0221 <evan_success@qq.com>
2016-09-22 19:57:11 +0800

    09/22/16 成功设置 tableView 的 sectionHeader. tableView 待数据接入. 调整版面, 去掉分享图标

--- blt/MyViewController.swift
+++ blt/MyViewController.swift
@@ -14,18 +14,43 @@ class MyViewController: UIViewController, UITableViewDelegate, UITableViewDataSo
         super.didReceiveMemoryWarning()
     }
     
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        
-//    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return CGFloat(30)
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let myHeaderView = tableView.dequeueReusableCell(withIdentifier: "myHeader")! as UITableViewCell
+        
+        let myLabel = myHeaderView.viewWithTag(1001) as! UILabel
+        
+        myLabel.text = "09/22/16"
+
+        
+        return myHeaderView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return CGFloat(0.0000001)
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return nil
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 1
+        return 4
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let myCell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
-        myCell.textLabel?.text = "123"
+        
+        myCell.textLabel?.text = "有一件事情要找你协商..."
+        myCell.imageView?.image = UIImage(named: "schedul")
         
         return myCell
     }
